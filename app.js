155b7778ff90944116e015902402c70cb964606a
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-07-06 14:10:53 +0100

    Add connection string

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const apiRouter = require("./routers/api.router");
-const { handleCustomErrors, handlePsqlErrors } = require("./controllers/errors.controllers");
+const {
+  handleCustomErrors,
+  handlePsqlErrors,
+  handleServerErrors,
+} = require("./controllers/errors.controllers");
 const app = express();
 
 app.use(express.json());
 
-app.use("/api", apiRouter)
+app.use("/api", apiRouter);
 
 app.all("*", (req, res) => {
-    res.status(404).send({msg: "Path Not Found"})
-})
+  res.status(404).send({ msg: "Path Not Found" });
+});
 
-app.use(handleCustomErrors)
-app.use(handlePsqlErrors)
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
